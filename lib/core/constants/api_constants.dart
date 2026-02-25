ffb411d0678ada5b6b87214647e205332e81c66b
smcheikhtourad <smcheikhtourad@gmail.com>
2026-02-25 12:49:44 +0000

    modification info.plist

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,8 +2,8 @@ class ApiConstants {
   ApiConstants._();
 
   // static const String baseUrl = "https://rough-mira-seen-app-f74aa423.koyeb.app/api/";
-  static const String baseUrl = "https://inmaa-api-gjhfcrfcg3hednhb.spaincentral-01.azurewebsites.net/api/";
-  //    static const String baseUrl ="http://192.168.100.13:5000/api/" ;
+  // static const String baseUrl = "https://inmaa-api-gjhfcrfcg3hednhb.spaincentral-01.azurewebsites.net/api/";
+     static const String baseUrl ="http://192.168.100.13:5000/api/" ;
   static const String properties= "${baseUrl}properties/";
   static const String banners= "${baseUrl}banners/";
   static const String notifications= "${baseUrl}notifications/";
